ce97b1d16161a98013001b05c3997df9a18aa004
PauloMoraiiis <paulodevmorais@gmail.com>
2022-05-26 15:11:05 -0300

    Validando tarefa ao usuario

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -76,7 +76,13 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
+        $user_id = auth()->user()->id;
+
+        if($tarefa->user_id == $user_id) {
         return view('tarefa.edit', ['tarefa' => $tarefa]);
+        }
+
+        return view('acesso-negado');
     }
 
     /**
@@ -88,6 +94,11 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
+
+        if(!$tarefa->user_id == auth()->user()->id) {
+            return view('acesso-negado');
+        }
+
         $tarefa->update($request->all());
         return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
     }
