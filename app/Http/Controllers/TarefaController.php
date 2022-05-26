8e9a424335873cb0ae6e12740a2ee2af81fba9b8
PauloMoraiiis <paulodevmorais@gmail.com>
2022-05-25 21:15:53 -0300

    email de nova tarefa

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Mail;
+use App\Mail\NovaTarefaMail;
 
 class TarefaController extends Controller
 {
@@ -71,6 +73,10 @@ class TarefaController extends Controller
     public function store(Request $request)
     {
         $tarefa = Tarefa::create($request->all());
+
+        $desrinatario = auth()->user()->email;
+        Mail::to($desrinatario)->send(new NovaTarefaMail($tarefa));
+
         return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
         
     }
@@ -83,7 +89,7 @@ class TarefaController extends Controller
      */
     public function show(Tarefa $tarefa)
     {
-        dd($tarefa->getAttributes());
+        return view('tarefa.show', ['tarefa' => $tarefa]);
     }
 
     /**
