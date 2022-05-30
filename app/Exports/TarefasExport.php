97515dbb10462dd21f67c37b96739be20c97294e
PauloMoraiiis <paulodevmorais@gmail.com>
2022-05-30 16:49:36 -0300

    Adicionando Titulo por meio do WithHeadings

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +15,9 @@ class TarefasExport implements FromCollection
     {
         return auth()->user()->tarefas()->get();
 
+    }
+
+    public function headings():array {
+        return ['Id da Tarefa', 'ID do Usuário', 'Tarefa', 'Data limite conclusão', 'Data criação', 'Data atualização'];
     }
 }
