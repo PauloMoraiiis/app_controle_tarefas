4044dd8c9dd277739344704d24740fa9a3e7b949
PauloMoraiiis <paulodevmorais@gmail.com>
2022-05-31 14:09:56 -0300

    Gerando view Dom/PDF

---
+++ resources/views/tarefa/pdf.blade.php
@@ -0,0 +1,22 @@
+<h2>Lista de tarefas</h2>
+
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach($tarefas as $key => $tarefa)
+        <tr>
+            <td>{{ $tarefa->id }}</td>
+            <td>{{ $tarefa->tarefa }}</td>
+            <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+        </tr>
+
+        @endforeach
+    </tbody>
+</table>
